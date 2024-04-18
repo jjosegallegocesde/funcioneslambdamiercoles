a2cbe9c463626521e6b7b9e85c0294a57e820875
yasury valencia <valenciahurtadoyasury@gmail.com>
2024-04-17 20:21:46 -0500

    nt9 feat se crea ejemplo de lamdba

---
+++ tradicional2.py
@@ -0,0 +1,5 @@
+# sofware de nomina
+#gestion de vacaciones
+# sumar la cantadida de un dia de vacaciones + dias libres ganado
+def sumarVacaciones(diasvacaciones,diaslibre):
+    return diasvacaciones+diaslibre
