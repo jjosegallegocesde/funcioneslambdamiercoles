7ee2b6a45aa211e8787a1a44d7199bd654c09fd7
jozeto4584 <jozetoz4584@gmail.com>
2024-04-17 19:31:00 -0500

    nt9 feat se crea la funcion login

---
+++ tradicional.py
@@ -0,0 +1,14 @@
+#DECLARAR UNA FUNCION
+def validarUsuario(userName, userPassword):
+    userDB ="user1234"
+    passwordDB ="admin123"
+    if userPassword==passwordDB and userName == userDB:
+        return True
+    else:
+        return False
+    
+    
+#LLAMANDO UNA FUNCION
+nombre="Jose"
+resultado= (validarUsuario("Juan","admin123"))   
+print(validarUsuario) 
