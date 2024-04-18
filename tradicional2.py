190c7dea000b484baf82ece35b295317cf77470b
aleja4arboleda <aleja.arboleda4@gmail.com>
2024-04-17 20:22:27 -0500

    nt9 feat se crea ejemplo lambda

---
+++ tradicional2.py
@@ -0,0 +1,9 @@
+#software de gestion de vaciones, sumar la cantidad de dias de vacaciones mas disa libres ganados
+
+def sumarVacaciones(diasVacaciones,diasLibres):
+   return diasVacaciones + diasLibres
+
+
+print(sumarVacaciones(2,1))
+
+
