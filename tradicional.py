37b1ae202290c32381787a87a4b28643c19669a4
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-17 19:30:59 -0500

    nt9 feat se crea funcion login

---
+++ tradicional.py
@@ -0,0 +1,13 @@
+#DECLARAR UNA FUNCION
+def validarUsuario(userName,userPassword):
+    userDB="user1234"
+    passwordDB="admin123"
+    if userPassword == passwordDB and userName == userDB:
+        return True
+    else:
+        return False
+
+# LLAMANDO LA FUNCION
+nombre="Jose"
+resultado=validarUsuario("Juan","admin123")
+print(resultado)
